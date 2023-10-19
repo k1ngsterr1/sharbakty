3a9001e26d8bb8881c3bf04d98630a4e9e6c190f
Ruslan Makhmatov <ruslanmakhmatov@gmail.com>
2023-10-19 23:15:29 +0600

    Version 1.0.4

--- src/components/Separator/Separator.tsx
+++ src/components/Separator/Separator.tsx
@@ -5,20 +5,26 @@ import { faWheatAwn } from "@fortawesome/free-solid-svg-icons";
 interface SeparatorProps {
   number: string;
   color?: string;
+  colorBg?: string;
   marginTop?: string;
 }
 
-const Separator: React.FC<SeparatorProps> = ({ number, marginTop, color }) => {
+const Separator: React.FC<SeparatorProps> = ({
+  number,
+  marginTop,
+  color,
+  colorBg,
+}) => {
   return (
     <div className={`separator-container ${marginTop}`}>
       <span className="icon-container">
         <FontAwesomeIcon
           icon={faWheatAwn}
-          className="wheat-icon"
+          className={`wheat-icon ${color}`}
         ></FontAwesomeIcon>
         <span className={`numeric ${color}`}>{number}</span>
       </span>
-      <figure className={`separator mt8 ${color}`}></figure>
+      <figure className={`separator mt8 ${colorBg}`}></figure>
     </div>
   );
 };
--- src/screens/ServicesScreen/ServicesScreen.tsx
+++ src/screens/ServicesScreen/ServicesScreen.tsx
@@ -1,15 +1,32 @@
 import React from "react";
 import Separator from "../../components/Separator/Separator";
+import { useNavigate } from "react-router-dom";
+
+interface LinkProps {
+  linkName: string;
+  number: string;
+  url: string;
+}
 
 const ServicesScreen = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="screen bg-blue">
       <section className="content">
-        <Separator number="03" color="white"></Separator>
-        <div className="heading-container right">
+        <Separator
+          number="03"
+          color="white"
+          colorBg="bg-white"
+          marginTop="mt32"
+        ></Separator>
+        <div className="heading-container center mt32">
           <h2 className="heading-s white">Наши услуги</h2>
-          <span className="wheat-subtext white">Sharbakty Bidai Terminal</span>
+          <span className="wheat-subtext white mt8">
+            Sharbakty Bidai Terminal
+          </span>
         </div>
+        <div className="links"></div>
       </section>
     </div>
   );
