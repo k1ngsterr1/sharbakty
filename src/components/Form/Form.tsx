8c7e3ae87bbbd75b773d23364841ac81ad04636b
Ruslan Makhmatov <ruslanmakhmatov@gmail.com>
2023-10-20 19:31:59 +0600

    Version 1.1.0

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -19,28 +19,62 @@ const Form = () => {
     { value: "Сушка + Приемка", label: "Сушка + Приемка" },
   ];
 
+  const customStyles = {
+    control: (provided: any, state: any) => ({
+      ...provided,
+      width: "100%",
+      color: "$blue", // Replace with your color code if $blue is not directly accessible here.
+      fontFamily: "Open Sans",
+      fontSize: "$font18mob", // Replace with your font size if $font18mob is not directly accessible here.
+      fontWeight: 300,
+      border: "none",
+      borderBottom: "2px solid #2D3A53",
+      boxShadow: "none",
+      "&:hover": {
+        borderColor: "none",
+      },
+    }),
+    dropdownIndicator: (provided: any, state: any) => ({
+      ...provided,
+      color: "$blue", // Replace with your color code.
+    }),
+    placeholder: (provided: any, state: any) => ({
+      ...provided,
+      color: "$blue",
+      textAlign: "left",
+    }),
+    singleValue: (provided: any, state: any) => ({
+      ...provided,
+      color: "$blue", // Replace with your color code.
+    }),
+  };
+
   return (
-    <form className="form">
+    <form className="form mt48">
       <div className="form-mobile-content">
         <input
           type="text"
           className="form-input"
           name="name_value"
           id="name"
+          placeholder="Ваше Имя"
           required={true}
         />
         <input
           type="text"
-          className="form-input"
+          className="form-input mt32"
           name="name_value"
           id="name"
+          placeholder="Ваш Телефон"
           required={true}
         />
         <Select
           options={options}
           placeholder="Выберите услугу"
-          className="select-styles"
+          className="select-styles mt32"
+          styles={customStyles}
         ></Select>
+        <FormButton marginTop="mt32" text="Отправить"></FormButton>
       </div>
     </form>
   );
